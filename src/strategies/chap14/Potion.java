24aafd022d5ac848e5f72b062c069895ace95423
inhoo <alyxforest@gmail.com>
2021-07-19 23:37:23 +0900

    선형 자료 구조 기하 문제 추가

--- src/strategies/chap14/Potion.java
+++ src/strategies/chap14/Potion.java
@@ -3,12 +3,10 @@ package strategies.chap14;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class Potion {
 
